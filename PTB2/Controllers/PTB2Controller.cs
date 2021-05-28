183e3b0cd54da04a322e20de8fb900f3142347d9
2 Thanh Vũ Thị <1821051103@student.humg.edu.vn>
2021-05-28 16:24:54 +0700

    Add project files.

---
+++ PTB2/Controllers/PTB2Controller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTB2.Controllers
+{
+    public class PTB2Controller : Controller
+    {
+        // GET: PTB2
+        public ActionResult GPT(String soa, String sob, String soc)
+        {
+            Double a, b, c, delta;
+            String ketqua = " ";
+            a = Convert.ToDouble(soa);
+            b = Convert.ToDouble(sob);
+            c = Convert.ToDouble(soc);
+            delta = b * b - 4 * a * c;
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    ketqua = ("Phuong Trinh Vo Nghiem");
+                }
+                else
+                {
+                    Double x = -c / b;
+                    ketqua = ("Vay nghiem cua phuong trinh la:  " + x);
+                }
+            }
+            else
+            {
+                if (delta < 0)
+                {
+                    ketqua = ("Vay phuong trinh vo nghiem");
+                }
+                else if (delta == 0)
+                {
+                    Double x11 = -b / 2 * a;
+                    ketqua = ("Phuong trinh co nghiem kep:  " + x11);
+                }
+                else if (delta > 0)
+                {
+                    Double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+                    Double x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+                    ketqua = ("Phuong trinh co hai nghiem phan biet: ");
+                    ketqua = ("Nghiem thu nhat: " + x1);
+                    ketqua = ("Nghiem thu hai: " + x2);
+                }
+
+            }
+            ViewBag.abc = ketqua;
+            return View();
+            }
+        }
+    }
